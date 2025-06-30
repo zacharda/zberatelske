f0c36022aac34e5d81a768d3d6713d386c5ba7b0
Dávid Zachar <davidzachar9@gmail.com>
2025-06-30 21:20:12 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,11 @@
 {#each data.groupedProducts as [year, products]}
   <div class="mb-6">
     <h2 class="text-xl font-bold mb-2">{year}</h2>
-    <hr class="mb-4 border-gray-300" />
+    <hr class="mb-4 border-black" />
 
-    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
       {#each products as product}
-        <div class="p-4 border rounded shadow">
+        <div class="p-4 border border-gray-300 rounded-3xl ">
           <img
             src={product.image_url}
             alt={product.name}
