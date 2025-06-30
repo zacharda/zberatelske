488ab490615e453cc5f119d10ef797242cb46030
Dávid Zachar <davidzachar9@gmail.com>
2025-06-30 20:45:33 +0200

    responsive images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,13 @@
 <div class="grid grid-cols-2 md:grid-cols-5 gap-4">
 	{#each data.products as product}
 		<div class="p-4 border rounded shadow">
-			<img
-				src={product.image_url}
-				alt={product.name}
-				class="max-w-[200px] aspect-square object-cover mb-2 rounded mx-auto"
-			/>
+			<div class="max-w-[200px] w-full mx-auto">
+				<img
+					src={product.image_url}
+					alt={product.name}
+					class="w-full aspect-square object-cover mb-2 rounded"
+				/>
+			</div>
 			<h2 class="font-semibold">{product.name}</h2>
 			<p class="text-sm">{product.year}</p>
 		</div>
